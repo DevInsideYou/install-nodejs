6c53f95903a650927f7e13b4fda191d523c73ca8
Vladyslav Pekker <agilesteel@gmail.com>
2020-05-24 13:26:01 -0300

    Update install-nodejs.sh

--- install-nodejs.sh
+++ install-nodejs.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-# install node
-sudo snap install --classic node --channel=12
-
-echo
-
-echo '"node" and "npm" are now on the path'
-
-echo
-
-# update node
-sudo snap refresh node
-
 # remove yourself
 rm $0
+
+# install curl
+sudo apt install -yqqq curl
+
+# install node
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+echo
+echo '"node" and "npm" are now on the path'
